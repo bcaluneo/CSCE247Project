6e2b33dd7b4b9098c878227fb3867c7e1ea52479
bcaluneo <caluneo.brendan@gmail.com>
2020-03-23 18:30:29 -0400

    Singleton's for UserManager and ShowDatabase.java

--- src/teamblue/ShowDatabase.java
+++ src/teamblue/ShowDatabase.java
@@ -1,5 +1,17 @@
 package teamblue;
 
 public class ShowDatabase {
+
+	private static ShowDatabase showDatabase;
 	
+	public static ShowDatabase getInstance() {
+		if (showDatabase == null) {
+			showDatabase = new ShowDatabase();
+		}
+		
+		return showDatabase;
+	}
+	
+	private ShowDatabase() {
+	}
 }
--- src/teamblue/UserManager.java
+++ src/teamblue/UserManager.java
@@ -2,4 +2,16 @@ package teamblue;
 
 public class UserManager {
 	
+	private static UserManager userManager;
+	
+	public static UserManager getInstance() {
+		if (userManager == null) {
+			userManager = new UserManager();
+		}
+		
+		return userManager;
+	}
+	
+	private UserManager() {
+	}
 }
