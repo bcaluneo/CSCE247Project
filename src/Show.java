a35f5c6a46094e78bd764d56e19086081047bfcf
bcaluneo <caluneo.brendan@gmail.com>
2020-03-23 23:14:02 -0400

    Started outlining skeleton classes.

--- src/Show.java
+++ src/Show.java
@@ -1,3 +1,12 @@
-public class Show {
-	
+public abstract class Show {
+
+	private ShowInformation showInformation;
+
+	public ShowInformation getShowInformation() {
+		return showInformation;
+	}
+
+	public void setShowInformation(ShowInformation showInformation) {
+		this.showInformation = showInformation;
+	}
 }
