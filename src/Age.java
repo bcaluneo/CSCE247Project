944e470acab06b59f00f85a74cc2dd4e15346e37
bcaluneo <caluneo.brendan@gmail.com>
2020-03-24 16:27:09 -0400

    Enums done.

--- src/Age.java
+++ src/Age.java
@@ -1,3 +1,3 @@
 public enum Age {
-	
+	G, PG, PG13, R, NC17, NR;
 }
--- src/Genre.java
+++ src/Genre.java
@@ -1,3 +1,10 @@
 public enum Genre {
-	
+	ADVACT, /* Adventure-Action */
+	COMEDY,
+	HORROR,
+	SCIFI,
+	ROMANCE,
+	THRILLER,
+	FANTASY,
+	OTHER;
 }
