339d218499fb2125ac5f054eff2ae581020ee2bb
Gohansupersaiyan12 <habershamfrank@yahoo.com>
2020-03-31 17:52:22 -0400

    Seat and Theater done

--- src/Seat.java
+++ src/Seat.java
@@ -1,3 +1,44 @@
 public class Seat {
+	private char seatRow;
+	private int seatColumn;
+	private boolean isHandicapped, isBooked;
+	
+	public Seat(char seatRow, int seatColumn) {
+		this.seatRow = seatRow;
+		this.seatColumn = seatColumn;
+		this.isHandicapped = this.isBooked = false;
+	}
+
+	public char getSeatRow() {
+		return seatRow;
+	}
+
+	public void setSeatRow(char seatRow) {
+		this.seatRow = seatRow;
+	}
+
+	public int getSeatColumn() {
+		return seatColumn;
+	}
+
+	public void setSeatColumn(int seatColumn) {
+		this.seatColumn = seatColumn;
+	}
+
+	public boolean isHandicapped() {
+		return isHandicapped;
+	}
+
+	public void setHandicapped(boolean isHandicapped) {
+		this.isHandicapped = isHandicapped;
+	}
+
+	public boolean isBooked() {
+		return isBooked;
+	}
+
+	public void setBooked(boolean isBooked) {
+		this.isBooked = isBooked;
+	}
 	
 }
--- src/Theater.java
+++ src/Theater.java
@@ -1,3 +1,65 @@
 public class Theater {
+	private Seat[][] seats;
+	
+	public Theater(char maxrows, int maxcolumns) {
+		seats = new Seat [maxrows-'A'+1][maxcolumns];
+		for(int rows=0; rows<seats.length; rows++)
+			for(int columns=0; columns<seats[0].length; columns++)
+				seats[rows][columns] = new Seat((char)('A'+rows),columns);
+	}
+	public void printSeats() {
+		int rows, columns;
+		System.out.print("R\\C");
+		for(columns=0; columns<seats[0].length; columns++)
+			System.out.print("  "+(columns+1)+" ");
+		System.out.println();
+		for(rows=0; rows<seats.length; rows++){
+			System.out.print("   -");
+			for(columns=0; columns<seats[0].length; columns++)
+				System.out.print("----");
+			System.out.println();
+			System.out.print(" "+(char)('A'+rows)+" ");
+			System.out.print("|");
+			for(columns=0; columns<seats[0].length; columns++){
+				System.out.print(" ");
+				if(seats[rows][columns].isBooked()) {
+					System.out.print("B");
+				} else if(seats[rows][columns].isHandicapped()) {
+					System.out.print("H");
+				}else {
+					System.out.print("U");
+				}
+				System.out.print(" |");
+			}
+			System.out.println();
+		}
+		System.out.print("   -");
+		for(columns=0; columns<seats[0].length; columns++)
+			System.out.print("----");
+		System.out.println();
+	}
+	
+	public boolean onlyHandicapped() {
+		for(int rows=0; rows<seats.length; rows++)
+			for(int columns=0; columns<seats[0].length; columns++)
+				if(!seats[rows][columns].isHandicapped() && !seats[rows][columns].isBooked())
+					return false;
+		return true;
+	}
+	
+	public boolean isFull() {
+		for(int rows=0; rows<seats.length; rows++)
+			for(int columns=0; columns<seats[0].length; columns++)
+				if(!seats[rows][columns].isBooked())
+					return false;
+		return true;
+	}
+	
+	public Seat[][] getSeats() {
+		return seats;
+	}
+	public void setSeats(Seat[][] seats) {
+		this.seats = seats;
+	}
 	
 }
