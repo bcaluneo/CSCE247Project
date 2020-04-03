4c15d3686e1eb03cdc2d4c54676c6c6cc6a991b7
ravipatel <ravipatel@localhost>
2020-04-02 21:54:39 -0400

    updated by Ravi

--- src/Payment.java
+++ src/Payment.java
@@ -1,3 +1,24 @@
+
 public class Payment {
+	private User user;
+	private Theater venue;
+	private Room room;
+	private Movie movie;
+	char seatRow;
+	int seatColumn;
 	
+	public Payement(User user, Theater Venue, Movie movie, Room room) {
+		this.user = user;
+		this.venue = venue;
+		this.movie = movie;
+		this.room = room;
+	}
+	
+	public void initiatePayment() {
+		
+	}
+	
+	public void cancelPayment() {
+		
+	}
 }
--- src/Review.java
+++ src/Review.java
@@ -1,3 +1,12 @@
 public class Review {
+	private User author;
+	String description;
+	int rating;
+	
+	public Review(User author, String description, int rating) {
+		this.author = author;
+		this.description = description;
+		this.rating = rating;
+	}
 	
 }
