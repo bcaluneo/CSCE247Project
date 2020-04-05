b9916a60402541f8706396a5cef8748afbc06167
Gohansupersaiyan12 <habershamfrank@yahoo.com>
2020-04-05 17:15:37 -0400

    edited by Frank

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class UserManager {
 
 	private static UserManager userManager;
-	private User[] users;
+	private ArrayList<User> users;
 	private boolean loggedIn;
 	private User currentUser;
-
+	Scanner scanner = new Scanner(System.in);
+	
 	private UserManager() {
-		users = new User[100];
+		users = new ArrayList<User>();
 		currentUser = new User();
 	}
 
@@ -22,26 +26,107 @@ public class UserManager {
 		return currentUser;
 	}
 
-	public void createUser(User user) {
+	public void createUser() {
+		User user = new User();
+		boolean Continue = true;
+		String string;
+		while(Continue){
+			System.out.println("Enter your email");
+			string = scanner.next();
+			if(emailExists(string)) {
+				System.out.println("This email is already in use.");
+			} else {
+				user.setProfileInformation("email", string);
+				Continue = false;
+			}
+		}
+		Continue = true;
+		while(Continue){
+			System.out.println("Enter your username");
+			string = scanner.next();
+			if(usernameExists(string)) {
+				System.out.println("This username is already in use.");
+			} else {
+				user.setProfileInformation("username", string);
+				Continue = false;
+			}
+		}
+		System.out.println("Create a password");
+		string = scanner.next();
+		user.setProfileInformation("password", string);
+		users.add(user);
+	}
+	
+	public boolean emailExists(String email) {
+		for(User user : users) {
+			if(user.getProfileInformation("email")==email) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean usernameExists(String username) {
+		for(User user : users) {
+			if(user.getProfileInformation("username")==username) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	public void deleteUser(User user) {
+	public void deleteUser(String email) {
+		if(emailExists(email)){
+			int account = 0;
+			for(User user : users) {
+				if(user.getProfileInformation("email")==email) {
+					users.remove(account);
+				}
+				account++;
+			}
+		}
 	}
 
-	public void loginUser(User user) {
+	public void loginUser() {
+		User user = new User();
+		boolean Continue = true;
+		String string;
+		while(Continue){
+			System.out.println("Enter your email");
+			string = scanner.next();
+			if(emailExists(string)) {
+				user = getUserByEmail(string);
+				Continue = false;
+			} else {
+				System.out.println("Email is invalid");
+			}
+		}
+		Continue=true;
+		while(Continue){
+			System.out.println("Enter your password");
+			string = scanner.next();
+			if(string==user.getProfileInformation("password")) {
+				this.currentUser=user;
+				Continue = false;
+			} else {
+				System.out.println("Password is invalid");
+			}
+		}
 	}
 
-	public void logoutUser(User user) {
+	public void logoutUser() {
+		this.currentUser=null;
 	}
 
-	public User getUserByName(String username) {
+	public User getUserByEmail(String email) {
+		for(User user : users) {
+			if(user.getProfileInformation("email")==email) {
+				return user;
+			}
+		}
 		return null;
 	}
 	
-	public static void main(String[] args) {
-		ShowUI showUI= new ShowUI();
-		showUI.run();
-	}
 }
 
 
