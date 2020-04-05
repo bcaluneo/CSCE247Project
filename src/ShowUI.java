1d3449d76eb11767dc84720a223a1f50f6dc02c5
bcaluneo <caluneo.brendan@gmail.com>
2020-04-05 00:03:48 -0400

    Created basic console interface.

--- src/ShowUI.java
+++ src/ShowUI.java
@@ -4,17 +4,59 @@ public class ShowUI {
 
 	private Scanner scanner;
 
-	private String[] options = {"Login", "Create Account", "Search", "Book"};
+	private String[] options = {"Login", "Create Account", "Search", "Book",
+															"Quit"};
 
 	public ShowUI() {
 		scanner = new Scanner(System.in);
 	}
 
+	public void printOptions() {
+		System.out.println("===========================");
+		System.out.println("Options:");
+		for (int i = 0; i < options.length; i++) {
+				String printout = options[i];
+				System.out.println(i + ": " + printout);
+		}
+
+		System.out.print("Make a selection: ");
+	}
+
+	public int getInput() {
+		printOptions();
+		int selection = scanner.nextInt();
+		while (selection < 0 || selection >= options.length) {
+			printOptions();
+			selection = scanner.nextInt();
+		}
+
+		return selection;
+	}
+
 	public void run() {
 		System.out.println("Show System Started");
 
-		// boolean interrupted = false;
-		// while (!interrupted) {
-		// }
+		boolean quit = false;
+		while (!quit) {
+			int selection = getInput();
+			switch (selection) {
+				case 0:
+					selection = getInput();
+					break;
+				case 1:
+					selection = getInput();
+					break;
+				case 2:
+					selection = getInput();
+					break;
+				case 3:
+					selection = getInput();
+					break;
+				case 4:
+					System.out.println("Quitting system...");
+					quit = true;
+					break;
+			}
+		}
 	}
 }
