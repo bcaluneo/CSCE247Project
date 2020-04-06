89997fab8100449380ec228be2b3ed0e4f045269
bcaluneo <caluneo.brendan@gmail.com>
2020-04-06 13:40:27 -0400

    Fixed Show class.

--- src/Show.java
+++ src/Show.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Show {
 
 	private static final int MAX_ATTRIBUTES = 7;
 
 	private HashMap<String, Object> showInformation;
+	private List<Review> reviews;
 
 	public Show() {
 		this.showInformation = new HashMap<String, Object>(MAX_ATTRIBUTES);
@@ -16,6 +19,8 @@ public class Show {
 		showInformation.put("genre", null); /* Enum (Genre) */
 		showInformation.put("inTheaters", null); /* boolean */
 		showInformation.put("times", null); /* String[] */
+
+		reviews = new ArrayList<Review>();
 	}
 
 	public Object getShowInformation(String key) {
@@ -30,5 +35,15 @@ public class Show {
 		}*/
 
 		showInformation.replace(key, value);
+	}
+
+	public double getAverageRating() {
+		return -1;
+	}
+
+	public void addReview(Review review) {
+	}
+
+	public void deleteReview(Review review) {
 	}
 }
