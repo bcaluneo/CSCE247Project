03e582451fe6ef2fa02c14e14c6b15dab1eaed3f
ravipatel <ravipatel@localhost>
2020-04-07 16:07:25 -0400

    added javadoc

--- src/Admin.java
+++ src/Admin.java
@@ -1,6 +1,11 @@
-
+/** 
+ * Inherit from Base Class User
+ * */
 public class Admin extends User {
 	
+	/**
+	 * this method removes inappropriate reviews 
+	 */
 	public void removeReview() {
 		
 	}
